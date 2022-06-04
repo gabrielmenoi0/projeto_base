82df6656f352d7eedb83e46a6f45589cf50f127c
jpmgoncalves12 <jpmgoncalves12@gmail.com>
2022-06-03 22:18:05 -0300

    Removendo comentários

--- lib/app.dart
+++ lib/app.dart
@@ -24,14 +24,15 @@ class _AppState extends State<App> {
       DeviceOrientation.portraitDown,
     ]);
 
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-        statusBarColor: Color.fromRGBO(57, 49, 153, 1),
-        statusBarBrightness: Brightness.light,
-        statusBarIconBrightness: Brightness.light));
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+          statusBarColor: Color.fromRGBO(57, 49, 153, 1),
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.light),
+    );
 
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: DefaultTheme.themeData,
       home: Splash(),
     );
   }
